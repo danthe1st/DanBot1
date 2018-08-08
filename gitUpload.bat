19e685a30989105d4494fd99c1ee48d1a78248ee
danthe1st <daniel@wwwmaster.at>
2018-08-08 15:10:21 +0200

    15:10:21,53

--- gitUpload.bat
+++ gitUpload.bat
@@ -2,10 +2,10 @@
 set REP_PATH=https://github.com/danthe1st/DanBot1.git
 ::cd GIT
 
-git remote add origin %REP_PATH%
+::git remote add origin %REP_PATH%
 git add .
 
 git commit -m %TIME%"
 git push -u origin --all -f
-git remote remove origin
+::git remote remove origin
 pause>nul
