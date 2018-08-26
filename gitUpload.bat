964cea2050c5207552d6f6b5ca40b161d53af196
dan1st <daniel@wwwmaster.at>
2018-08-26 10:45:43 +0200

    added more Colors for multicolor feature, fixed issue#1 and issue#2

--- gitUpload.bat
+++ gitUpload.bat
@@ -6,12 +6,13 @@ git remote add origin %REP_PATH%
 git add .
 
 echo enter Commit name:
-if x%var%==x (
+set /P var=
+if "x%var%"=="x" (
 	::empty
 	set var=%TIME%
 )
 
-git commit -m %TIME%"
+git commit -m "%var%"
 git push -u origin --all -f
 git remote remove origin
 pause>nul
