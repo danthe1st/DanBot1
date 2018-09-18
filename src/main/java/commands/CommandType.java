79fd47e70be3846b251680c755b9e908a5399566
danthe1st <daniel@wwwmaster.at>
2018-09-18 08:06:05 +0200

    8:06:05,10

--- src/main/java/commands/CommandType.java
+++ src/main/java/commands/CommandType.java
@@ -1,10 +1,11 @@
 package commands;
 
 public enum CommandType {
-	ADMIN("Admin Command - only bot-admin"),
-	BOT_MODERATION("DanBot1 Moderation Command"),
+	
 	USER("User Command"),
-	GUILD_MODERATION("Discord Server Moderation Command");
+	BOT_MODERATION("DanBot1 Moderation Command"),
+	GUILD_MODERATION("Discord Server Moderation Command"),
+	ADMIN("Admin Command - only bot-admin");
 	
 	private String name;
 	private CommandType(String type) {
