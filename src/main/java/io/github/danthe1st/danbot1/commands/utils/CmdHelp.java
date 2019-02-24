49dcea994065ed8c81ef286d89ed893c23b069c6
danthe1st <daniel@wwwmaster.at>
2019-02-24 12:18:57 +0100

    fixed JAR, modified Updater in order to use github, created docs folder for github pages

--- src/main/java/io/github/danthe1st/danbot1/commands/utils/CmdHelp.java
+++ src/main/java/io/github/danthe1st/danbot1/commands/utils/CmdHelp.java
@@ -36,19 +36,22 @@ public class CmdHelp implements Command{
 			
 			commands.forEach((name,command)->{
 				EmbedBuilder backUp=new EmbedBuilder(eb);
-				String help="**"+STATIC.getPrefix(event.getGuild()) +name+"**:\n"+command.help(STATIC.getPrefixExcaped(event.getGuild()))+"\n\n";
-				try {
-					eb.appendDescription(help);
-					if (!(eb.isValidLength(AccountType.CLIENT)/*&&eb.isValidLength(AccountType.BOT)*/)) {
+				if (command.getCommandType()==null) {
+					String help="**"+STATIC.getPrefix(event.getGuild()) +name+"**:\n"+command.help(STATIC.getPrefixExcaped(event.getGuild()))+"\n\n";
+					try {
+						eb.appendDescription(help);
+						if (!(eb.isValidLength(AccountType.CLIENT)/*&&eb.isValidLength(AccountType.BOT)*/)) {
+							event.getAuthor().openPrivateChannel().complete().sendMessage(backUp.build()).queue();
+							eb.clear();
+							eb.appendDescription(help);
+						}	
+					} catch (IllegalArgumentException e) {
 						event.getAuthor().openPrivateChannel().complete().sendMessage(backUp.build()).queue();
 						eb.clear();
 						eb.appendDescription(help);
-					}	
-				} catch (IllegalArgumentException e) {
-					event.getAuthor().openPrivateChannel().complete().sendMessage(backUp.build()).queue();
-					eb.clear();
-					eb.appendDescription(help);
+					}
 				}
+				
 				
 				
 			});
