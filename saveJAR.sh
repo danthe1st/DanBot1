4b7a514deff41870762abaf9d39fd5a0d15ce038
danthe1st <daniel@wwwmaster.at>
2019-07-27 17:32:00 +0200

    JAR in Wiki - automated by travis

---
+++ saveJAR.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git clone https://github.com/danthe1st/DanBot1.wiki.git wiki
+mvn package -DskipTests=true
+cp target/DanBot1.jar wiki/DanBot1.jar
+cd wiki
+git add DanBot1.jar
+git commit -m "CI JAR deploy: `date`"
+echo $githubUsername ; echo $githubToken | git push
