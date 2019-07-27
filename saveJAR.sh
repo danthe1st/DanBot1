3b317da964df31b608dc59802bf9bdbcfc8ddec6
danthe1st <daniel@wwwmaster.at>
2019-07-27 18:27:55 +0200

    github token in URL

--- saveJAR.sh
+++ saveJAR.sh
@@ -6,4 +6,4 @@ cp target/DanBot1.jar wiki/DanBot1.jar
 cd wiki
 git add DanBot1.jar
 git commit -m "CI JAR deploy: `date`"
-(echo $githubUsername ; echo $githubToken) | git push
+git push https://$githubUsername:$githubToken@github.com/danthe1st/DanBot1.wiki.git
