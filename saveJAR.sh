d1c98fa9ba7e3180b62742fff77f0139993b0c65
danthe1st <daniel@wwwmaster.at>
2019-09-05 12:30:43 +0200

    CI: deploy key for wiki deploy

--- saveJAR.sh
+++ saveJAR.sh
@@ -6,4 +6,4 @@ cp target/DanBot1.jar wiki/DanBot1.jar
 cd wiki
 git add DanBot1.jar
 git commit -m "CI JAR deploy: `date`"
-git push https://$githubUsername:$githubToken@github.com/danthe1st/DanBot1.wiki.git
+git push git@github.com:danthe1st/DanBot1.wiki.git
